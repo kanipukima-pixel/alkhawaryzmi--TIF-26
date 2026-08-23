3df674ddeda89acd3409f8ea652908739b0011a8
kanipukima-pixel <kanipukima@gmail.com>
2026-08-23 14:07:30 +0700

    Add files via upload

---
+++ 01.konsep_dasar_algoritma_pemrograman.py
@@ -0,0 +1,19 @@
+
+
+# Menampilkan Judul
+print("=======DATA DIRI MAHASISWA=======")
+# Menampilkan Nama Lengkap
+print("Nama                  : Alkhawaryzmi  Brilliant ")
+# Menampilkan NIM
+print("NIM                   : 2605060011")
+# Menampilkan Tempat, Tanggal Lahir
+print("Tempat, Tanggal Lahir : Purworejo, 10 Januari 2008")
+# Menampilkan Alamat
+print("Alamat                : Kab. Purworejo, Jawa Tengah")
+# Menampilkan Jenis Kelamin
+print("Jenis Kelamin         : Laki-laki")
+# Menampilkan Hobi
+print("Hobi                  : Bermain catur")
+
+
+
