396297519a8fea9792c30b0da91b8d0fa98d113b
kanipukima-pixel <kanipukima@gmail.com>
2026-08-29 10:34:18 +0700

    Add files via upload

---
+++ 0011_alkhawaryzmi_literaldata.py
@@ -0,0 +1,38 @@
+# ALKHAWARYZMI BRILLIANT
+# NPM  : 2605060011
+
+
+#1
+nama='alkhawaryzmi brilliant'
+umur= 18
+berat=66.5
+
+print(f'NAMA  : {nama}')
+print(f'UMUR  : {umur} tahun')
+print(f'BERAT : {berat} kg')
+
+#2
+
+angka_string = "123"
+angka_float = 45.67
+angka_integer = 89
+
+data_1 = int(angka_string)# 1. Konversi angka_string menjadi integer
+data_2= int(angka_float)# 2. Konversi angka_float menjadi integer
+data_3= float(angka_integer)# 3. Konversi angka_integer menjadi float
+data_4 = str(angka_integer)# 4. Konversi angka_integer menjadi string
+
+print(data_1,type(data_1))
+print(data_2,type(data_2))
+print(data_3,type(data_3))
+print(data_4,type(data_4))
+
+#3
+
+usia = int(input("Masukkan usia Anda: "))
+tinggi_badan = float(input("Masukkan tinggi badan Anda (dalam meter): "))
+nama = input("Masukkan nama Anda: ")
+
+print(f"Usia Anda: {usia} tahun")
+print(f"Tinggi badan Anda: {tinggi_badan} meter")
+print(f"Nama Anda: {nama}")
