d1a88a3b7d85fcd1fcc4f0584f50e1df97320aaa
kanipukima-pixel <kanipukima@gmail.com>
2026-08-31 18:21:47 +0700

    Refactor and update variable data handling

--- 0011_alkhawaryzmi_literaldata.py
+++ 0011_alkhawaryzmi_literaldata.py
@@ -2,7 +2,7 @@
 # NPM  : 2605060011
 
 
-#1
+#1 variabel dan tipe data
 nama='alkhawaryzmi brilliant'
 umur= 18
 berat=66.5
@@ -11,7 +11,7 @@ print(f'NAMA  : {nama}')
 print(f'UMUR  : {umur} tahun')
 print(f'BERAT : {berat} kg')
 
-#2
+#2 mengubah tipe data
 
 angka_string = "123"
 angka_float = 45.67
@@ -27,11 +27,11 @@ print(data_2,type(data_2))
 print(data_3,type(data_3))
 print(data_4,type(data_4))
 
-#3
+#3 input dari user
 
-usia = int(input("Masukkan usia Anda: "))
-tinggi_badan = float(input("Masukkan tinggi badan Anda (dalam meter): "))
-nama = input("Masukkan nama Anda: ")
+usia = int(input("Masukkan usia Anda: ")) # input tipe integer
+tinggi_badan = float(input("Masukkan tinggi badan Anda (dalam meter): ")) #input tipe float
+nama = input("Masukkan nama Anda: ") # input tipe string
 
 print(f"Usia Anda: {usia} tahun")
 print(f"Tinggi badan Anda: {tinggi_badan} meter")
